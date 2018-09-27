f24034364bfc42d65b055230f0384d96c5bb1ea9
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2018-09-27 17:20:00 +0300

    Added ContinueWhenAll to make tasks parallels

--- FictionLoadingApp/Program.cs
+++ FictionLoadingApp/Program.cs
@@ -6,32 +6,129 @@ using System.Threading.Tasks;
 
 namespace FictionLoadingApp
 {
-    //Все константы
-    public class Consts
+
+
+    public class Program
     {
-        //Screens
-        public const string divisionString = "------------------------------";
-        public const string splashScreenString = "Splash Screen";
-        public const string menuScreenString = "Menu Screen";
-        //Licece
-        public const string licenceRequesting = "Requesting Licence...";
-        public const string licenceRequestingEnd = "Licence Requesting is finished!";
-        public const string YESlicence = "Your program is licenced";
-        public const string NOlicence = "Your program is not licenced";
-        //Update
-        public const string updateRequesting = "Requesting Update...";
-        public const string updateRequestingEnd = "Update Requesting is finished!";
-        public const string YESupdate = "Your program is updated";
-        public const string NOupdate = "Your program is not updated";
-        public const string updateIsHere = "There is an update, now it will be downloaded";
-        public const string updateIsNotHere = "There is no update";
-        public const string updateDownloading = "Downloading update...";
-        public const string updateDownloadingFinished = "Downloading update finished!";
-        //Intenet
-        public const string NOinternet = "There's no internet connection, so it is not available to check your license and update";
-        public const string CheckInternet = "Watching if there is an internet...";
-        public const string YESinternet = "Internet connection established";
-        public const string NOInternet = "No signal";
+
+
+        //static bool internetConnection = true;
+        //static bool isThereALicence = true;
+        //static bool isThereAnUpdate = true;
+
+        static bool internetConnection = NextBool(80);
+        static bool isThereALicence = NextBool();
+        static bool isThereAnUpdate = NextBool(60);
+
+        static WelcomeScreen menu = new WelcomeScreen();
+
+        static void Main(string[] args)
+        {
+            Task menuprint = new Task(() =>
+            {
+                Console.Clear();
+                menu.DrawMenu();
+            });
+
+            ShowSplash();
+
+            InternetCheck();
+
+            if (menu.internetConnection)
+            {
+                Task licence = new Task(() => RequestLicence());
+                Task update = new Task(() => RequestUpdate());
+                licence.Start();
+                update.Start();
+                var continuation = Task.Factory.ContinueWhenAll(new[] { licence, update }, tasks => menuprint.Start());
+            }
+            else
+            {
+                menuprint.Start();
+            }
+
+            Console.ReadKey();
+        }
+
+        //Принимает количество процентов (1-100) на выпадение true
+        public static bool NextBool(int truePercentage = 50)
+        {
+            Random r = new Random();
+            return r.NextDouble() < truePercentage / 100.0;
+        }
+
+        //Проверка интернета
+        static void InternetCheck()
+        {
+            Console.WriteLine(Consts.CheckInternet);
+            Thread.Sleep(3000);
+            if (internetConnection)
+            {
+                menu.internetConnection = true;
+                Console.WriteLine(Consts.YESinternet);
+            }
+            else
+            {
+                menu.internetConnection = false;
+                Console.WriteLine(Consts.NOInternet);
+                Thread.Sleep(3000);
+            }
+        }
+
+        //Запрос лицензии
+        static void RequestLicence()
+        {
+            Console.WriteLine(Consts.licenceRequesting);
+            Thread.Sleep(5000);
+            if (isThereALicence)
+            {
+                menu.isThereALicence = true;
+            }
+            else
+            {
+                menu.isThereALicence = false;
+            }
+            Console.WriteLine(Consts.licenceRequestingEnd);
+            Thread.Sleep(3000);
+        }
+
+        //Запрос обновления
+        static void RequestUpdate()
+        {
+            Console.WriteLine(Consts.updateRequesting);
+            Thread.Sleep(2000);
+            Console.WriteLine(Consts.updateRequestingEnd);
+
+            if (isThereAnUpdate)
+            {
+                menu.isThereAnUpdate = true;
+                Console.WriteLine(Consts.updateIsHere);
+                Update();
+            }
+            else
+            {
+                menu.isThereAnUpdate = false;
+                Console.WriteLine(Consts.updateIsNotHere);
+                Thread.Sleep(3000);
+            }
+        }
+
+        //Обновление
+        static void Update()
+        {
+            Console.WriteLine(Consts.updateDownloading);
+            Thread.Sleep(10000);
+            Console.WriteLine(Consts.updateDownloadingFinished);
+            Thread.Sleep(3000);
+        }
+
+        //Отрисовка Splash Screen
+        static void ShowSplash()
+        {
+            Console.WriteLine(Consts.splashScreenString);
+            Console.WriteLine(Consts.divisionString);
+        }
+
     }
 
     //Меню
@@ -72,105 +169,34 @@ namespace FictionLoadingApp
 
         }
 
-
     }
 
-    public class Program
+    //Все константы
+    public class Consts
     {
-
-        static bool internetConnection = true;
-        static bool isThereALicence = false;
-        static bool isThereAnUpdate = true;
-
-        static WelcomeScreen menu = new WelcomeScreen();
-
-        static void Main(string[] args)
-        {
-            ShowSplash();
-            InternetCheck();
-
-            if (menu.internetConnection)
-            {
-                RequestLicence();
-                RequestUpdate();
-            }
-            
-            Console.Clear();
-            menu.DrawMenu();
-            Console.ReadKey();
-        }
-
-        //Проверка интернета
-        static void InternetCheck()
-        {
-            Console.WriteLine(Consts.CheckInternet);
-            Thread.Sleep(3000);
-            if (internetConnection)
-            {
-                menu.internetConnection = true;
-                Console.WriteLine(Consts.YESinternet);
-            }
-            else
-            {
-                menu.internetConnection = false;
-                Console.WriteLine(Consts.NOInternet);
-                Thread.Sleep(3000);
-            }
-        }
-
-        //Запрос лицензии
-        static void RequestLicence()
-        {
-            Console.WriteLine(Consts.licenceRequesting);
-            Thread.Sleep(3000);
-            Console.WriteLine(Consts.licenceRequestingEnd);
-            if (isThereALicence)
-            {
-                menu.isThereALicence = true;
-            }
-            else
-            {
-                menu.isThereALicence = false;
-            }
-        }
-
-        //Запрос обновления
-        static void RequestUpdate()
-        {
-            Console.WriteLine(Consts.updateRequesting);
-            Thread.Sleep(3000);
-            Console.WriteLine(Consts.updateRequestingEnd);
-
-            if (isThereAnUpdate)
-            {
-                menu.isThereAnUpdate = true;
-                Console.WriteLine(Consts.updateIsHere);
-                Update();
-            }
-            else
-            {
-                menu.isThereAnUpdate = false;
-                Console.WriteLine(Consts.updateIsNotHere);
-                Thread.Sleep(3000);
-            }
-        }
-
-        //Обновление
-        static void Update()
-        {
-            Console.WriteLine(Consts.updateDownloading);
-            Thread.Sleep(3000);
-            Console.WriteLine(Consts.updateDownloadingFinished);
-            Thread.Sleep(3000);
-        }
-
-        //Отрисовка Splash Screen
-        static void ShowSplash()
-        {
-            Console.WriteLine(Consts.splashScreenString);
-            Console.WriteLine(Consts.divisionString);
-        }
-
+        //Screens
+        public const string divisionString = "------------------------------";
+        public const string splashScreenString = "Splash Screen";
+        public const string menuScreenString = "Menu Screen";
+        //Licece
+        public const string licenceRequesting = "Requesting Licence...";
+        public const string licenceRequestingEnd = "Licence Requesting is finished!";
+        public const string YESlicence = "Your program is licenced";
+        public const string NOlicence = "Your program is not licenced";
+        //Update
+        public const string updateRequesting = "Requesting Update...";
+        public const string updateRequestingEnd = "Update Requesting is finished!";
+        public const string YESupdate = "Your program is updated";
+        public const string NOupdate = "Your program is not updated";
+        public const string updateIsHere = "There is an update, now it will be downloaded";
+        public const string updateIsNotHere = "There is no update";
+        public const string updateDownloading = "Downloading update...";
+        public const string updateDownloadingFinished = "Downloading update finished!";
+        //Intenet
+        public const string NOinternet = "There's no internet connection, so it is not available to check your license and update";
+        public const string CheckInternet = "Watching if there is an internet...";
+        public const string YESinternet = "Internet connection established";
+        public const string NOInternet = "No signal";
     }
 
 
