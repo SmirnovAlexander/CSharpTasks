9914d564e17c9c3f78cb416bb8e486343cb8069c
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2018-10-05 19:18:16 +0300

    Add project files.

---
+++ LinqTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
