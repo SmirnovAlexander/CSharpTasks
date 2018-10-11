2ec6bab1809eff2899ac70e2419be80488417b40
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2018-10-11 19:22:10 +0300

    Add project files.

---
+++ PerfectNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PerfectNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
