60b62efd19204bc8b2d19bf95d9dbeec49c6e7c3
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-06-13 18:54:36 +0300

    Added comments and writed calculations to another class

---
+++ PerfectNumbers/PerfectNumbers/Calculations.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PerfectNumbers
+{
+    /// <summary>
+    /// Plinq and linq variants of calculation perfect numbers.
+    /// </summary>
+    public class Calculations
+    {
+        // Counting using Plinq.
+        public static List<int> PlinqPerfectNumbers(int number) {
+           
+            //Initialising a numbers list
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < number; i++)
+                numbers.Add(i);
+
+            return (from nb in numbers.AsParallel() where CheckNumber(number) select number).ToList();
+        }
+
+        // Counting using linq.
+        public static List<int> LinqPerfectNumbers(int number)
+        {
+
+            //Initialising a numbers list
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < number; i++)
+                numbers.Add(i);
+
+            return (from nb in numbers where CheckNumber(number) select number).ToList();
+        }
+
+        // Check if the number is perfect.
+        public static bool CheckNumber(int num)
+        {
+            int sum = 1;
+            for (int i = 2; i < num / 2 + 1; i++)
+                if (num % i == 0)
+                    sum += i;
+            return (sum == num);
+        }
+    }
+}
---
+++ PerfectNumbers/PerfectNumbers/PerfectNumbers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace PerfectNumbers
+{
+    /// <summary>
+    /// Class for calculating time and printing it to console.
+    /// </summary>
+    class PerfectNumbers
+    {
+        static void Main(string[] args)
+        {
+            // Two lists with results.
+            List<int> perfectNumbers1 = new List<int>();
+            List<int> perfectNumbers2 = new List<int>();
+
+            // Initialising stopwatches.
+            Stopwatch sw1 = new Stopwatch();
+            Stopwatch sw2 = new Stopwatch();
+
+            // No parallel method.
+            Task taskA = Task.Run(() =>
+            {
+                Console.WriteLine("LINQ way starts...");
+                sw1.Start();
+                perfectNumbers1 = Calculations.LinqPerfectNumbers(10000);
+                sw1.Stop();
+                Console.WriteLine("LINQ way ended.");
+            });
+
+            // Parallel method.
+            Task taskB = Task.Run(() =>
+            {
+                Console.WriteLine("PLINQ way starts...");
+                sw2.Start();
+                perfectNumbers2 = Calculations.PlinqPerfectNumbers(10000);
+                sw2.Stop();
+                Console.WriteLine("PLINQ way ended.");
+            });
+
+            // Printing results.
+            Task finalTask = Task.Factory.ContinueWhenAll(new Task[] { taskA, taskB }, ant =>
+            {
+                Console.WriteLine("Numbers: ");
+                foreach (var number in perfectNumbers1)
+                    Console.WriteLine(number);
+                Console.WriteLine("Time of LINQ: " + sw1.ElapsedMilliseconds);
+                Console.WriteLine("Time of PLINQ: " + sw2.ElapsedMilliseconds);
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
